26bb0fb6b36f45af767a52e32213c3afa38d1acc
Maanas Sehgal <maanas.sehgal@gmail.com>
2023-12-30 03:33:46 +0530

    Added even.java

---
+++ Doubts/even.java
@@ -0,0 +1,16 @@
+package Doubts;
+
+import java.util.*;
+
+public class even {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
+    int n = sc.nextInt();
+    
+    System.out.println(n % 2 == 0 ? "The number is even" : "The number is odd");
+    sc.close();
+  }
+}
