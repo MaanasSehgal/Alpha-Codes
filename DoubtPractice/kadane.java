6eaccfee68f92d9b4b1c912391d23fe514d641d4
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-01-17 02:14:53 +0530

    Solved kadane

---
+++ DoubtPractice/kadane.java
@@ -0,0 +1,25 @@
+package DoubtPractice;
+
+public class kadane {
+
+    public static int kadaneAlg(int arr[]) {
+        int n = arr.length;
+        int currSum = 0, maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            currSum += arr[i];
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(currSum, maxSum);
+        }
+        return maxSum;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 6, -2, 3, -7, -5, 4, 7, -8, -6, 10, -4 };
+
+        System.out.println(kadaneAlg(arr));
+    }
+}
