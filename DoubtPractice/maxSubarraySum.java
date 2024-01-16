bcad72ebf6bf1611f99814e4d4d5ca13b1195cbc
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-01-17 00:18:00 +0530

    Adding practice questions

---
+++ DoubtPractice/maxSubarraySum.java
@@ -0,0 +1,24 @@
+package DoubtPractice;
+
+public class maxSubarraySum {
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 5, -2, 3, -7, -1, 2, 12, 1, -2 };
+        // int arr[] = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        int maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int currSum = 0;
+                for (int k = i; k <= j; k++) {
+                    currSum += arr[k];
+                }
+
+                maxSum = Math.max(currSum, maxSum);
+            }
+        }
+
+        System.out.print("The max sum is: " + maxSum);
+    }
+}
---
+++ DoubtPractice/prefixArray.java
@@ -0,0 +1,31 @@
+package DoubtPractice;
+
+public class prefixArray {
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 5, -2, 3, -7, -1, 2, 12, 1, -2 };
+        int n = arr.length;
+
+        //prefix/running sum of array
+        int prefix[] = new int[n];
+        prefix[0] = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i];
+        }
+
+        //max subarray sum
+
+        int maxSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currSum = 0;
+            for (int j = i; j < n; j++) {
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+            }
+            maxSum = Math.max(currSum, maxSum);
+        }
+
+        System.out.println("Max sum by prefix is: " + maxSum);
+    }
+}
