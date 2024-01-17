836973c68865eef4181201e49ef078926b5467c5
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-01-17 18:22:15 +0530

    Added homework problems

---
+++ Lec9/hw1.java
@@ -0,0 +1,38 @@
+package Lec9;
+
+import java.util.Arrays;
+
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct
+public class hw1 {
+
+    //Approach 1
+    public static boolean checkDuplicate(int nums[]) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Approach 2
+    public static boolean checkDuplicate2(int nums[]) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        int nums[] = { 1, 2, 3, 4, 1 };
+        System.out.println(checkDuplicate2(nums));
+    }
+}
---
+++ Lec9/hw3.java
@@ -0,0 +1,31 @@
+package Lec9;
+
+public class hw3 {
+
+    public static int calculateMaxProfit(int prices[]) {
+        int n = prices.length;
+
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currPrice = prices[i];
+            if (buyPrice < currPrice) {
+                int currProfit = currPrice - buyPrice;
+
+                maxProfit = Math.max(currProfit, maxProfit);
+            } else {
+                buyPrice = currPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        // int prices[] = { 7, 1, 5, 3, 6, 4 };
+        int prices[] = {7, 6, 4, 3, 1};
+
+        int maxProfit = calculateMaxProfit(prices);
+        System.out.println("Maximum profit: " + maxProfit);
+    }
+}
