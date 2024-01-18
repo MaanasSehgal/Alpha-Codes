cf1e0d424d191eb96e0873201e21330bcf41c77e
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-01-18 21:48:22 +0530

    Practiced sorting

---
+++ Important/sortingAgain.java
@@ -0,0 +1,62 @@
+package Important;
+
+public class sortingAgain {
+
+    public static void printArray(int arr[], int n) {
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void insertionSort(int arr[], int n) {
+        for (int i = 1; i < n; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 4, 2, 1, 6, 7, 5, 3 };
+        int n = arr.length;
+
+        // bubbleSort(arr, n);
+        // selectionSort(arr, n);
+        // insertionSort(arr, n);
+        printArray(arr, n);
+    }
+}
