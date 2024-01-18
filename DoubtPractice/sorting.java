8f31baa3079f83d16eb0b941909d9674fed64f75
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-01-18 18:45:21 +0530

    Adding sorting practice file

---
+++ DoubtPractice/sorting.java
@@ -0,0 +1,52 @@
+package DoubtPractice;
+
+import Lec10.insertionSort;
+
+public class sorting {
+
+    public static void bubbleSortArr(int arr[]) {
+        int n = 5;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void selectionSortArr(int arr[]) {
+        int n = 5;
+
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min] > arr[j])  {
+                    min = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        System.out.println();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        selectionSortArr(arr);
+
+        printArr(arr);
+    }
+}
