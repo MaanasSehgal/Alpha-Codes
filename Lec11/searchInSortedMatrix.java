94cd4b36a150b44ca477d951f5b2918a5a6e4974
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-01-22 02:48:29 +0530

    Completed all approaches for searching in sorted matrix

--- Lec11/searchInSortedMatrix.java
+++ Lec11/searchInSortedMatrix.java
@@ -4,14 +4,26 @@ import java.util.Scanner;
 
 public class searchInSortedMatrix {
 
+    //brute force method, TC: O(n ^ 2)
+    public static void bruteForce(int matrix[][], int m, int n, int key) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println(key + " found at index (" + i + ", " + j + ")");
+                    return;
+                }
+            }
+        }
+        System.out.println(key + " wasn't found in given matrix");
+    }
+
+    //Staircase method, TC: O(m + n)
     public static void searchKey(int matrix[][], int m, int n, int key) {
         int row = 0, col = n - 1;
 
         while (row < m && col >= 0) {
             if (matrix[row][col] == key) {
-                System.out.println(
-                    key + " found at index (" + row + ", " + col + ")"
-                );
+                System.out.println(key + " found at index (" + row + ", " + col + ")");
                 return;
             } else if (key < matrix[row][col]) {
                 col--;
@@ -19,7 +31,40 @@ public class searchInSortedMatrix {
                 row++;
             }
         }
-        System.out.println(key + "wasn't found in given matrix");
+        System.out.println(key + " wasn't found in given matrix");
+    }
+
+    //BINARY SEARCH APPROACH, TC: O(n log n)
+
+    //binary search algorithm
+    public static boolean binarySearch(int arr[], int key, int i) {
+        int n = arr.length;
+        int start = 0, end = n - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == key) {
+                System.out.println(key + " found at index (" + i + ", " + mid + ")");
+                return true;
+            }
+
+            if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void binarySearchApproach(int matrix[][], int m, int n, int key) {
+        for (int i = 0; i < m; i++) {
+            if (binarySearch(matrix[i], key, i)) {
+                return;
+            }
+        }
+        System.out.println("Key not found");
     }
 
     public static void main(String[] args) {
@@ -42,6 +87,8 @@ public class searchInSortedMatrix {
 
         sc.close();
 
+        // bruteForce(matrix, m, n, key);
+        // binarySearchApproach(matrix, m, n, key);
         searchKey(matrix, m, n, key);
     }
 }
