c1c46d7d9c1a81199e38653a78e761c9f40a777e
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-05-08 01:52:23 +0530

    Added trie codes

---
+++ BluePrints/Trie.java
@@ -0,0 +1,18 @@
+package BluePrints;
+
+public class Trie {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Lec32/PrefixProblem.java
@@ -0,0 +1,67 @@
+package Lec32;
+
+public class PrefixProblem {
+
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        public Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) { // O(L)
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static boolean search(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+        return curr.eow;
+    }
+
+    public static boolean wordBreak(String key) {
+        if (key.length() == 0) {
+            return true;
+        }
+
+        for (int i = 1; i <= key.length(); i++) {
+            // substring(beg, last excluded)
+            if (search(key.substring(0, i)) &&
+                    wordBreak(key.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
+        String key = "ilikesamsung";
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+
+        System.out.println(wordBreak(key));
+    }
+}
