0e029475d2992e089355575af22a301fa0bf2da2
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-06-19 01:07:33 +0530

    Added fibonacci using dp

--- Lec37/Fibonacci.java
+++ Lec37/Fibonacci.java
@@ -1,19 +1,38 @@
 package Lec37;
 
 public class Fibonacci {
-    public static int fib(int n, int[] f) {
+    public static int fibRecursion(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibRecursion(n - 1) + fibRecursion(n - 2);
+    }
+
+    public static int fibMemoization(int n, int[] f) {
         if (n == 0 || n == 1) {
             return n;
         }
         if (f[n] != 0) {// fib(n) is already calculated
             return f[n];
         }
-        return f[n] = fib(n - 1, f) + fib(n - 2, f);
+        return f[n] = fibMemoization(n - 1, f) + fibMemoization(n - 2, f);
+    }
+
+    public static int fibTabulation(int n) {
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 
     public static void main(String[] args) {
         int n = 6; // 0 1 1 2 3 5
         int f[] = new int[n + 1];
-        System.out.println(fib(n, f));
+        System.out.println(fibRecursion(n));
+        System.out.println(fibMemoization(n, f));
+        System.out.println(fibTabulation(n));
     }
 }
